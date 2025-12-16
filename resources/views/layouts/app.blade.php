1292e7ca1b2e3fff07a8601b84cbc3c78a6368b8
freakyworkers <kakeclass@gmail.com>
2025-12-17 01:31:39 +0900

    Fix @else block by removing PHP echo from :root - use direct CSS syntax

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -315,14 +315,15 @@
                 color: var(--header-text-color) !important;
             }
         @else
-            <?php echo "\n            :root {\n"; ?>
+            <?php echo "\n"; ?>
+            :root {
                 --header-text-color: {{ $colorLightHeaderText }};
                 --header-bg-color: {{ $colorLightHeaderBg }};
                 --body-text-color: {{ $colorLightBodyText }};
                 --body-bg-color: {{ $colorLightBodyBg }};
                 --point-main-color: {{ $colorLightPointMain }};
                 --point-bg-color: {{ $colorLightPointBg }};
-            <?php echo "\n            }\n"; ?>
+            }
             body {
                 background-color: var(--body-bg-color);
                 color: var(--body-text-color);
