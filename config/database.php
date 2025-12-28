7987e091a081e38f4e6fb81ec139924eefd91f42
freakyworkers <kakeclass@gmail.com>
2025-12-28 20:23:58 +0900

    Fix Redis cache database to 0 for ElastiCache Serverless cluster mode

--- config/database.php
+++ config/database.php
@@ -86,7 +86,7 @@ return [
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'database' => env('REDIS_CACHE_DB', '0'),
             'scheme' => env('REDIS_SCHEME', 'tcp'),
         ],
 
